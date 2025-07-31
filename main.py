65a81d26b3b637c2844ba139007d91612639f0b8
amerbakri <94579195+amerbakri@users.noreply.github.com>
2025-07-31 19:20:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import subprocess
 import re
 import logging
 import asyncio
-import functoolsfrom datetime import datetime, timezone, timedelta
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import functools
+from datetime import datetime, timezone, timedelta
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
